7bc1dcee511137d8d3b05e6e8d0e34ef34f9bba6
Adrienne <adriennehl36@gmail.com>
2019-09-21 12:36:54 -0500

    add bookmark test

--- save_bookmarks.js
+++ save_bookmarks.js
@@ -1,20 +1,19 @@
+alert("Hi.");
 // callback function for add-button Listener, Place your bookmarker 
 // need to find a way to get title and x and y coordinates
-function add_bookmark(event){
-    // turn on listener for paragraph click
-    $("p").click(function(){
-        alert("The paragraph was clicked.");
-        // $(this).slideUp();
-        // create a new bookmark object
-        var new_bookmark = {'title': title, 'x_coord': event.clientX, 
+function add_bookmark(){
+    alert("button clicked!");
+    // turn on listener for document click
+    document.addEventListener('click', function(event){
+        console.log(event.target);
+        alert("The page was clicked.");
+        var new_bookmark = {'x_coord': event.clientX, 
             'y_coord': event.clientY};
         console.log("x_coord: " + x_coord + " y_coord: " + y_coord);
         // sent bookmark to local data
-        localStorage.setItem(title, JSON.stringify(new_bookmark));
-
+        localStorage.setItem('new_bookmark', JSON.stringify(new_bookmark));
     });
-
-    // localData =  JSON.parse(localStorage.getItem('title'));
 }
-var addButton = document.getElementById("add-button")
-addButton.addEventListener('click', add_bookmark)
+
+// localData =  JSON.parse(localStorage.getItem('title'));
+document.getElementsByName("add-button")[0].addEventListener('click', add_bookmark);
