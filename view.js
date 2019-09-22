5a6ff3ea9e76dcb8bbf10ad7593d48cb976d8ded
KC Luo <you@example.com>
2019-09-21 19:19:53 -0500

    Added div_add function

--- view.js
+++ view.js
@@ -3,5 +3,37 @@ $(function(){
     //绑定点击事件
     $("#btn1").click(function(){
         $("#div1").after("<font color='red' size='7'>Test</font>");
+    this.$OuterDiv = $(document.createElement("div"))
+        .hide()
+        .append($('<table></table>')
+            .attr({ cellSpacing : 0 })
+            .addClass("text")
+        )
+    ;
     });
 });
+
+createEl = function(t, a, y, x)//编写建立一个新对象的通用方法
+    { 
+        var e = document.createElement(t);
+        document.body.appendChild(e); //漏了这一句，否则行不通
+        if (a) { 
+            for (var k in a) { 
+                if (k == 'class') e.className = a[k]; 
+                else if (k == 'id') e.id = a[k]; 
+        else e.setAttribute(k, a[k]); 
+            } 
+        }
+        if (y) { for (var k in y) e.style[k] = y[k]; } 
+        if (x) { e.appendChild(document.createTextNode(x)); } 
+        return e;
+    }
+
+//再通过以下方法来进行调用建立一个新层
+function Button2_onclick() //先把建立一个新的DIV的方法写成一个通用方法，然后通过调用方法实例化建立DIV
+{var newElement = createEl('div', 
+    {'class': 'newDivClass', id: 'newDiv', name: 'newDivName'},
+    {width: '300px', height:'200px', margin:'0 auto', border:'1px solid #DDD'}, 
+    'Add contents here');
+
+}
