8ac7ee7c16e7226be5e8014de94c78162e2ec6f6
Adrienne <adriennehl36@gmail.com>
2019-09-22 07:26:00 -0500

    main2 edits

--- main2.js
+++ main2.js
@@ -1,10 +1,12 @@
 chrome.runtime.onMessage.addListener(
     function(request) {
-    alert("message recieved");
+//    alert("message recieved");
+//    alert(request);
     var info = JSON.parse(request);
     x_coord = info.x_coord;
     y_coord = info.y_coord;
     url = info.url;
+    alert("x_coord " + x_coord + " y_coord " + y_coord + " url " + url);
     window.location.assign(url);
     window.scrollTo(x_coord, y_coord);
 });
