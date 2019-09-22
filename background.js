534886e8a7008237d979806db6861d6b78373f30
Adrienne <adriennehl36@gmail.com>
2019-09-22 01:42:00 -0500

    passes coordinates of scroll from content to background

--- background.js
+++ background.js
@@ -2,9 +2,18 @@ chrome.runtime.onInstalled.addListener(function(){
     alert("this is the background script");
 });
 
+var coords1, coords2;
 function runContentScript(){
     chrome.tabs.executeScript({file: 'main.js'});
 }
+
+chrome.runtime.onMessage.addListener(
+    function(request) {
+    var coords = JSON.parse(request);
+    alert(coords.y_coord);
+    coords1 = coords.x_coord;
+    coords2 = coords.y_coord;
+    });
 // tells content script to scroll
 /***chrome.commands.onCommand.addListener(function (command) {
     if (command == "scroll") {
--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 x_coord = window.pageXOffset;
 y_coord = window.pageYOffset;
+// display x and y coord to screen 
 alert("cs: x_coord: " + x_coord + " y_coord: " + y_coord);
-// save_bookmark(x_coord, y_coord);
+
+// send x_coord and y_coord to save_bookmarks
+function save_bookmark(x_co, y_co){
+    var new_bookmark = {'x_coord': x_co, 
+        'y_coord': y_co};
+    //document.getElementById("x-position").value = new_bookmark.x_co;
+    //document.getElementById("y-position").value = new_bookmark.y_co;
+    chrome.runtime.sendMessage(JSON.stringify({"x_coord": x_co, "y_coord": y_co}));
+}
+
+save_bookmark(x_coord, y_coord);
+
+
+
 
 // main.js
 /***
