b24c63014fc3a5b13edd4d58d3b7a121a5aeca6e
Adrienne <adriennehl36@gmail.com>
2019-09-21 22:45:39 -0500

    xy to scrolling form

--- save_bookmarks.js
+++ save_bookmarks.js
@@ -2,12 +2,16 @@
 // need to find a way to get title and x and y coordinates
 $(function(){
     $("#add-button").click(function(){
-        alert("The button was clicked.");
-        document.addEventListener('dblclick', save_bookmark);
+        alert("The place your bookmarker button was clicked.");
+        //document.addEventListener('dblclick', save_bookmark);
+        x_coord = window.pageXOffset;
+        y_coord = window.pageYOffset;
+        alert("x_coord: " + x_coord + " y_coord: " + y_coord);
+        save_bookmark(x_coord, y_coord);
     });
 
     $("#submit").click(function(){
-        alert('it works!');
+        alert('submit the bookmark!');
         url = document.getElementById("url").value;
         title = document.getElementById("title").value;
         keywords = document.getElementById("keywords").value;
@@ -17,22 +21,25 @@ $(function(){
         time = document.getElementById("date").value;
         var submission = {'url': url, 'title': title, 'keywords': keywords, 'notes': notes, "xpos": xpos,
         "ypos": ypos};
+        alert(submission + url + title + keywords + notes + xpos +ypos);
         localStorage.setItem(time, JSON.stringify(submission));
         console.log(localStorage);
     });
 });
 
 
-function save_bookmark(event){
-    console.log(event.target);
-    alert("The page was clicked.");
-    var new_bookmark = {'x_coord': event.clientX, 
-        'y_coord': event.clientY};
+function save_bookmark(x_coord, y_coord){
+    //console.log(event.target);
+    //alert("The page was clicked.");
+    //var new_bookmark = {'x_coord': event.clientX, 
+    //    'y_coord': event.clientY};
+    var new_bookmark = {'x_coord': x_coord, 
+        'y_coord': y_coord};
     console.log("x_coord: " + new_bookmark.x_coord + " y_coord: " + new_bookmark.y_coord);
     var dt = new Date();
     document.getElementById("date").value = dt.toISOString();
     document.getElementById("x-position").value = new_bookmark.x_coord;
     document.getElementById("y-position").value = new_bookmark.y_coord;
-    document.removeEventListener('dblclick', save_bookmark);
+    // document.removeEventListener('dblclick', save_bookmark);
 }
 
