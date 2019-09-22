36c921732e0b408188a731f2c34eadc4855d2c07
KC Luo <you@example.com>
2019-09-22 00:00:37 -0500

    Eabled dynamic div generation

--- view.js
+++ view.js
@@ -23,14 +23,15 @@ $(function(){
 
     var data_f = []
     for (var i = 0; i < unique_WP.length; i++){
-        data_f.push([unique_WP[i],[],[]]);
+        data_f.push([unique_WP[i],[],0,[]]);
     }
 
     for (var i = 0; i < data.length; i++){
         for (var j = 0; j < data_f.length; j++){
             if(JSON.parse(data[i][0]).url === data_f[j][0]){
                 data_f[j][1] = JSON.parse(data[i][0]).title;
-                data_f[j][2].push([data[i][1],
+                data_f[j][2] += 1;
+                data_f[j][3].push([data[i][1],
                     JSON.parse(data[i][0]).keywords,
                     JSON.parse(data[i][0]).notes, 
                     JSON.parse(data[i][0]).xpos,
@@ -39,12 +40,26 @@ $(function(){
             }
         }
     }
-    document.getElementById("demo").innerHTML = data_f;
+    
+    //document.getElementById("demo").innerHTML = data_f[1];
+
+    for (var i = 0; i < data_f.length; i++){
+        var title = data_f[i][1];
+        var url = data_f[i][0];
+        //window.alert(title);
+        var newElement = createEl('div', 
+        {'class': 'WP', id: url, name: title},
+        {width: '98%', height:'100px', margin:'3px', border:'1px solid #DDD'}, 
+        title);
+    };
+
+
 });
 
 
 
-
+//再通过以下方法来进行调用建立一个新层
+//先把建立一个新的DIV的方法写成一个通用方法，然后通过调用方法实例化建立DIV
 createEl = function(t, a, y, x)//编写建立一个新对象的通用方法
     { 
         var e = document.createElement(t);
@@ -54,18 +69,9 @@ createEl = function(t, a, y, x)//编写建立一个新对象的通用方法
                 if (k == 'class') e.className = a[k]; 
                 else if (k == 'id') e.id = a[k]; 
         else e.setAttribute(k, a[k]); 
-            } 
+            }
         }
         if (y) { for (var k in y) e.style[k] = y[k]; } 
         if (x) { e.appendChild(document.createTextNode(x)); } 
         return e;
     }
-
-//再通过以下方法来进行调用建立一个新层
-function Button_onclick() //先把建立一个新的DIV的方法写成一个通用方法，然后通过调用方法实例化建立DIV
-{var newElement = createEl('div', 
-    {'class': 'newDivClass', id: 'newDiv', name: 'newDivName'},
-    {width: '300px', height:'200px', margin:'0 auto', border:'1px solid #DDD'}, 
-    'Add contents here');
-
-}
